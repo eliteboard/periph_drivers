f52886517059f8a460e0720cca7cf706e2d0e8f0
Reinhard Feger <47209718+rf-eng@users.noreply.github.com>
2020-05-15 21:16:32 +0200

    initial commit isl28023 driver by alex kaineder

--- pwr_meas/isl28023.c
+++ pwr_meas/isl28023.c
@@ -1,1 +1,286 @@
-/* Driver for power measurement chip ISL28023 */
+/**
+  ******************************************************************************
+  * @file           : ISL28023.c
+  * @brief          : functions for communication with the power monitor
+  * 					ISL28023
+  ******************************************************************************
+  * @attention
+  *
+  * @author    Alexander Kaineder
+  * @date      Jul 29, 2019
+  *
+  ******************************************************************************
+  */
+
+#include "ISL28023.h"
+#include "Stm_Platform.h"
+
+WS_DPM WsDpm;
+
+/**
+  * @brief Initialization of I2C handle
+  */
+void dpm_init(void)
+{
+	WsDpm.DpmI2cHandle = &hi2c1;				/* DPM I2C handle = SMBUS2 handle */
+	WsDpm.Tx.bufRp = 0;						/* init tx buffer */
+	WsDpm.Tx.bufWp = 0;
+	WsDpm.Tx.bufUsed = 0;
+	WsDpm.smTx = SM_DPM_IDLE;
+	WsDpm.busStat = DPM_READY;
+}
+
+/**
+  * @brief Service routine for I2C data transfer
+  * 	   This routine calls the transmit and receive handler.
+  * 	   The routine has to be called periodically from the main loop at high rate.
+  * @retval none
+  */
+void dpm_service(void)
+{
+	dpm_transmit();
+
+	dpm_receive();
+
+}
+
+/**
+  * @brief Write configuration data to dpm in non blocking mode.
+  *        This function writes n bytes to the device addressed with devAddr.
+  *        The data is written to a circular buffer which is automatically
+  *        transferred to the DPM by "dpm_transmit()".
+  * @param phost: I2C handle pointer defined in "dpm_init"
+  * @param devNum: Device number
+  * @param devAddr: Device address
+  * @param regAddr: Register address
+  * @param buffer: Pointer to transmit data buffer
+  * @param size: Number of bytes to be transferred (regAddr not included)
+  * @retval status
+  */
+DPM_STAT dpm_writeBuf(uint8_t devAddr, uint8_t regAddr, uint8_t *pData, uint8_t size)
+{
+	int i_n;
+	if (size+1 > DPM_TX_BUFFER_LEN-1 - WsDpm.Tx.bufUsed)
+		return DPM_BUFFER_FULL;
+
+	WsDpm.Tx.buffer[WsDpm.Tx.bufWp++] = devAddr;
+	WsDpm.Tx.bufUsed++;
+	if (WsDpm.Tx.bufWp >= DPM_TX_BUFFER_LEN)
+		WsDpm.Tx.bufWp = 0;
+	WsDpm.Tx.buffer[WsDpm.Tx.bufWp++] = size+1;
+	WsDpm.Tx.bufUsed++;
+	if (WsDpm.Tx.bufWp >= DPM_TX_BUFFER_LEN)
+		WsDpm.Tx.bufWp = 0;
+	WsDpm.Tx.buffer[WsDpm.Tx.bufWp++] = regAddr;
+	WsDpm.Tx.bufUsed++;
+	if (WsDpm.Tx.bufWp >= DPM_TX_BUFFER_LEN)
+		WsDpm.Tx.bufWp = 0;
+	for (i_n = 0; i_n < size; i_n++)
+	{
+		WsDpm.Tx.buffer[WsDpm.Tx.bufWp++] = pData[i_n];
+		WsDpm.Tx.bufUsed++;
+		if (WsDpm.Tx.bufWp >= DPM_TX_BUFFER_LEN)
+			WsDpm.Tx.bufWp = 0;
+	}
+	return DPM_OK;
+}
+
+/**
+  * @brief Write configuration data to dpm in blocking mode.
+  *        This function writes n bytes to the device addressed with devAddr.
+  * @param phost: I2C handle pointer defined in "dpm_init"
+  * @param devNum: Device number
+  * @param devAddr: Device address
+  * @param regAddr: Register address
+  * @param buffer: Pointer to transmit data buffer
+  * @param size: Number of bytes to be transferred (regAddr not included)
+  * @retval status
+  */
+DPM_STAT dpm_write(uint8_t devAddr, uint8_t regAddr, uint8_t *pData, uint8_t size)
+{
+	int i_n;
+	uint8_t data[5];
+
+	WsDpm.busStat = DPM_BUSY;
+	data[0] = regAddr;
+	for (i_n = 0; i_n < size; i_n++)
+		data[i_n+1] = pData[i_n];
+	if (HAL_I2C_Master_Transmit(WsDpm.DpmI2cHandle, devAddr, data, size+1, 100) != HAL_OK)
+	{
+		WsDpm.busStat = DPM_READY;
+		return DPM_ERROR;
+	}
+	while (HAL_I2C_GetState(WsDpm.DpmI2cHandle) != HAL_I2C_STATE_READY) {}
+
+	WsDpm.busStat = DPM_READY;
+	return DPM_OK;
+}
+/**
+  * @brief Read measurement data from dpm in non blocking mode.
+  *        This function reads n bytes from the device addressed with devAddr.
+  *        The device address and register address is written to "WsDpm.halRx.".
+  *        When the Rx state-machine is in idle mode, it is directly started
+  *        and the data is transferred by "dpm_receive" when the i2c bus is free.
+  *        After the transfer is finished the status is in "WsDpm.halRx.status".
+  * @param phost: I2C handle pointer defined in "dpm_init"
+  * @param devNum: Device number
+  * @param devAddr: Device address
+  * @param regAddr: Register address
+  * @param buffer: Pointer to receive data buffer
+  * @param size: Number of bytes to be transferred (regAddr not included)
+  * @retval status
+  */
+DPM_STAT dpm_read(uint8_t devAddr, uint8_t regAddr, uint8_t *pData, uint8_t size)
+{
+	if ((WsDpm.smRx == SM_DPM_IDLE) && (WsDpm.busStat == DPM_READY))
+	{
+		WsDpm.halRx.devAddr = devAddr;
+		WsDpm.halRx.size = size;
+		WsDpm.halRx.regAddr = regAddr;
+		WsDpm.halRx.data = pData;
+		WsDpm.halRx.status = DPM_BUSY;
+		WsDpm.smRx = SM_DPM_BUS_READY;
+		WsDpm.busStat = DPM_BUSY;
+		return DPM_OK;
+	}
+	else
+	{
+		return DPM_BUSY;
+	}
+}
+
+
+/**
+  * @brief Receive data from DPM
+  *        This function controls the transfer of the Tx.buffer content to the I2C hal routines.
+  * @retval none
+  */
+void dpm_receive(void)
+{
+	switch (WsDpm.smRx)
+	{
+	case SM_DPM_IDLE:
+		break;
+
+	case SM_DPM_BUS_READY:
+		if (HAL_I2C_GetState(WsDpm.DpmI2cHandle) == HAL_I2C_STATE_READY)
+			WsDpm.smRx = SM_DPM_START;
+		break;
+
+	case SM_DPM_START:
+//		if (HAL_I2C_Mem_Read(WsDpm.DpmI2cHandle, WsDpm.halRx.devAddr, WsDpm.halRx.regAddr, I2C_MEMADD_SIZE_8BIT, WsDpm.halRx.data, WsDpm.halRx.size, 100) == HAL_OK)
+		if (HAL_I2C_Mem_Read_DMA(WsDpm.DpmI2cHandle, WsDpm.halRx.devAddr, WsDpm.halRx.regAddr, I2C_MEMADD_SIZE_8BIT, WsDpm.halRx.data, WsDpm.halRx.size) == HAL_OK)
+			WsDpm.smRx = SM_DPM_RX_READY;
+		else
+		{
+			WsDpm.halRx.status = DPM_ERROR;
+			WsDpm.smRx = SM_DPM_END;
+		}
+		break;
+
+	case SM_DPM_RX_READY:
+		if (HAL_I2C_GetState(WsDpm.DpmI2cHandle) == HAL_I2C_STATE_READY)
+		{
+			WsDpm.halRx.status = DPM_READY;
+			WsDpm.smRx = SM_DPM_END;
+		}
+		break;
+
+	case SM_DPM_END:
+		WsDpm.busStat = DPM_READY;
+		WsDpm.smRx = SM_DPM_IDLE;
+		break;
+
+	default:
+		WsDpm.smRx = SM_DPM_IDLE;
+		break;
+	}
+}
+
+
+/**
+  * @brief Transmit commands from Tx.buffer
+  *        This function controls the transfer of the Tx.buffer content to the I2C hal routines.
+  * @retval none
+  */
+void dpm_transmit(void)
+{
+	int i_n;
+	uint8_t data[5];
+
+	switch (WsDpm.smTx)
+	{
+	case SM_DPM_IDLE:
+		if (WsDpm.Tx.bufUsed > 0 && WsDpm.busStat == DPM_READY)
+		{
+			WsDpm.busStat = DPM_BUSY;
+			WsDpm.smTx = SM_DPM_BUS_READY;
+		}
+		break;
+
+	case SM_DPM_BUS_READY:
+		if (HAL_I2C_GetState(WsDpm.DpmI2cHandle) == HAL_I2C_STATE_READY)
+			WsDpm.smTx = SM_DPM_START;
+		break;
+
+	case SM_DPM_START:
+		WsDpm.halTx.devAddr = WsDpm.Tx.buffer[WsDpm.Tx.bufRp++];
+		WsDpm.Tx.bufUsed--;
+		if (WsDpm.Tx.bufRp >= DPM_TX_BUFFER_LEN)
+			WsDpm.Tx.bufRp = 0;
+		WsDpm.halTx.size = WsDpm.Tx.buffer[WsDpm.Tx.bufRp++];
+		WsDpm.Tx.bufUsed--;
+		if (WsDpm.Tx.bufRp >= DPM_TX_BUFFER_LEN)
+			WsDpm.Tx.bufRp = 0;
+		WsDpm.halTx.data = data;
+		for (i_n = 0; i_n < WsDpm.halTx.size; i_n++)
+		{
+			WsDpm.halTx.data[i_n] = WsDpm.Tx.buffer[WsDpm.Tx.bufRp++];
+			WsDpm.Tx.bufUsed--;
+			if (WsDpm.Tx.bufRp >= DPM_TX_BUFFER_LEN)
+				WsDpm.Tx.bufRp = 0;
+		}
+//		if (HAL_I2C_Master_Transmit_DMA(WsDpm.DpmI2cHandle, WsDpm.halTx.devAddr, WsDpm.halTx.data, WsDpm.halTx.size) == HAL_OK)
+		if (HAL_I2C_Master_Transmit(WsDpm.DpmI2cHandle, WsDpm.halTx.devAddr, WsDpm.halTx.data, WsDpm.halTx.size, 100) == HAL_OK)
+			WsDpm.smTx = SM_DPM_END;
+		else
+			WsDpm.smTx = SM_DPM_BUS_FREE;
+		break;
+
+	case SM_DPM_BUS_FREE:
+		if (HAL_I2C_GetState(WsDpm.DpmI2cHandle) == HAL_I2C_STATE_READY)
+			WsDpm.smTx = SM_DPM_RETRANS;
+		break;
+
+	case SM_DPM_RETRANS:
+//		if (HAL_I2C_Master_Transmit_DMA(WsDpm.DpmI2cHandle, WsDpm.halTx.devAddr, WsDpm.halTx.data, WsDpm.halTx.size) == HAL_OK)
+		if (HAL_I2C_Master_Transmit(WsDpm.DpmI2cHandle, WsDpm.halTx.devAddr, WsDpm.halTx.data, WsDpm.halTx.size, 100) == HAL_OK)
+			WsDpm.smTx = SM_DPM_END;
+		else
+			WsDpm.smTx = SM_DPM_END;
+		break;
+
+	case SM_DPM_END:
+		WsDpm.busStat = DPM_READY;
+		WsDpm.smTx = SM_DPM_IDLE;
+		break;
+
+	default:
+		break;
+	}
+}
+
+//	HAL_I2C_StateTypeDef HAL_I2C_GetState(I2C_HandleTypeDef *hi2c);
+//	HAL_I2C_ModeTypeDef  HAL_I2C_GetMode(I2C_HandleTypeDef *hi2c);
+
+//	HAL_StatusTypeDef HAL_I2C_Master_Transmit(I2C_HandleTypeDef *hi2c, uint16_t DevAddress, uint8_t *pData, uint16_t Size, uint32_t Timeout);
+//	HAL_StatusTypeDef HAL_I2C_Master_Transmit_IT(I2C_HandleTypeDef *hi2c, uint16_t DevAddress, uint8_t *pData, uint16_t Size);
+//	HAL_StatusTypeDef HAL_I2C_Master_Transmit_DMA(I2C_HandleTypeDef *hi2c, uint16_t DevAddress, uint8_t *pData, uint16_t Size);
+//	HAL_StatusTypeDef HAL_I2C_Master_Receive(I2C_HandleTypeDef *hi2c, uint16_t DevAddress, uint8_t *pData, uint16_t Size, uint32_t Timeout);
+//	HAL_StatusTypeDef HAL_I2C_Master_Receive_IT(I2C_HandleTypeDef *hi2c, uint16_t DevAddress, uint8_t *pData, uint16_t Size);
+//	HAL_StatusTypeDef HAL_I2C_Master_Receive_DMA(I2C_HandleTypeDef *hi2c, uint16_t DevAddress, uint8_t *pData, uint16_t Size);
+//	HAL_StatusTypeDef HAL_I2C_Mem_Read(I2C_HandleTypeDef *hi2c, uint16_t DevAddress, uint16_t MemAddress, uint16_t MemAddSize, uint8_t *pData, uint16_t Size, uint32_t Timeout);
+//	HAL_StatusTypeDef HAL_I2C_Mem_Read_IT(I2C_HandleTypeDef *hi2c, uint16_t DevAddress, uint16_t MemAddress, uint16_t MemAddSize, uint8_t *pData, uint16_t Size);
+//	HAL_StatusTypeDef HAL_I2C_Mem_Read_DMA(I2C_HandleTypeDef *hi2c, uint16_t DevAddress, uint16_t MemAddress, uint16_t MemAddSize, uint8_t *pData, uint16_t Size);
+
+
--- pwr_meas/isl28023.h
+++ pwr_meas/isl28023.h
@@ -1,1 +1,175 @@
-/* Driver for power measurement chip ISL28023 */
+/**
+  ******************************************************************************
+  * @file           : ISL28023.h
+  * @brief          : definitions for the digital power monitor (DPM) ISL28023
+  ******************************************************************************
+  * @attention
+  *
+  * @author    Alexander Kaineder
+  * @date      Jul 29, 2019
+  *
+  ******************************************************************************
+  */
+
+#ifndef ISL28023_H_
+#define ISL28023_H_
+
+#include "main.h"
+
+
+/* Device register definition -------------------------------------------------------*/
+// device info
+#define REG_CAPABILITY			0x19
+#define REG_VOUT_MODE			0x20
+#define REG_PMBUS_REV			0x99
+#define REG_IC_DEVICE_ID		0xAD
+#define REG_IC_DEVICE_REV		0xAE
+// global control
+#define REG_RESTORE_DEFAULT		0x12
+#define REG_OPERATEN			0x01
+// channel control
+#define REG_SET_DPM_MODE		0xD2
+#define REG_DPM_CONV_SATUS		0xD3
+#define REG_CONFIG_ICHANNEL		0xD4
+#define REG_IOUT_CAL_GAIN		0x38
+#define REG_CONFIG_VCHANNEL		0xD5
+#define REG_CONFIG_PEAK_DET		0xD7
+#define REG_CONFIG_EXCITATION	0xE2
+// measurement register
+#define REG_READ_VSHUNT_OUT		0xD6
+#define REG_READ_VOUT			0x8B
+#define REG_READ_IOUT			0x8C
+#define REG_READ_PEAK_MIN_IOUT	0xD8
+#define REG_READ_PEAK_MAX_IOUT 	0xD9
+#define REG_READ_POUT			0x96
+#define REG_READ_VSHUNT_OUT_AUX	0xE0
+#define REG_READ_VOUT_AUX		0xE1
+#define REG_READ_TEMPERATURE_1	0x8D
+// threshold detector
+#define REG_VOUT_OV_THRES_SET	0xDA
+#define REG_VOUT_UV_THRES_SET	0xDB
+#define REG_IOUT_OC_THRES_SET	0xDC
+// smb alert
+#define REG_CONFIG_INTR			0xDD
+#define REG_FORCE_FEEDTHR_ALERT	0xDE
+#define REG_SMBALERT_MASK		0x1B
+#define REG_SMBALERT2_MASK		0xDF
+#define REG_CLEAR_FAULTS		0x03
+#define REG_STATUS_VOUT			0x7A
+#define REG_STATUS_IOUT			0x7B
+#define REG_STATUS_TEMPERATURE	0x7D
+#define REG_STATUS_CML			0x7E
+#define REG_STATUS_BYTE			0x78
+#define REG_STATUS_WORD			0x79
+// voltage margin
+#define REG_CONFIG_VOL_MARGIN	0xE4
+#define REG_SET_VOL_MARGIN		0xE3
+// external clock control
+#define REG_CONFIG_EXT_CLK		0xE5
+
+/* Register value definition -------------------------------------------------------*/
+// Conversion time in us
+#define DPM_CONV_TIME_64		0x0
+#define DPM_CONV_TIME_128		0x1
+#define DPM_CONV_TIME_256		0x2
+#define DPM_CONV_TIME_512		0x3
+#define DPM_CONV_TIME_1024		0x5
+#define DPM_CONV_TIME_2048		0x7
+// Number of samples for average
+#define DPM_AVG_SAMPL_1			0x0
+#define DPM_AVG_SAMPL_2			0x1
+#define DPM_AVG_SAMPL_4			0x2
+#define DPM_AVG_SAMPL_8			0x3
+#define DPM_AVG_SAMPL_16		0x4
+#define DPM_AVG_SAMPL_32		0x5
+#define DPM_AVG_SAMPL_64		0x6
+#define DPM_AVG_SAMPL_128		0x7
+#define DPM_AVG_SAMPL_264		0x8
+#define DPM_AVG_SAMPL_512		0x9
+#define DPM_AVG_SAMPL_1024		0xA
+#define DPM_AVG_SAMPL_2048		0xB
+#define DPM_AVG_SAMPL_4096		0xC
+// Shift position for bit setting
+#define DPM_AUX_AVG				10
+#define DPM_AUX_CONV			7
+#define DPM_PRI_AVG				3
+#define DPM_PRI_CONV			0
+
+/* Communication buffer definition -------------------------------------------------------*/
+#define DPM_TX_BUFFER_LEN		128
+
+typedef enum {
+	DPM_OK,
+	DPM_BUSY,
+	DPM_READY,
+	DPM_ERROR,
+	DPM_BUFFER_FULL
+} DPM_STAT;
+
+typedef struct DPM_BUF
+{
+	uint8_t *data;
+	uint8_t devAddr;
+	uint8_t regAddr;
+	uint8_t size;
+	DPM_STAT status;
+
+} DPM_BUF;
+
+typedef struct DPM_RBUF
+{
+	uint8_t buffer[DPM_TX_BUFFER_LEN];					/* transmit buffer */
+	uint16_t bufWp; 									/* write pointer */
+	uint16_t bufRp; 									/* read pointer */
+	uint16_t bufUsed; 									/* number of used bytes in buffer  */
+} DPM_RBUF;
+
+/* Definition for state-machine  ----------------------------------------------*/
+typedef enum {
+	SM_DPM_IDLE,
+	SM_DPM_BUS_READY,
+	SM_DPM_START,
+	SM_DPM_BUS_FREE,
+	SM_DPM_RX_READY,
+	SM_DPM_RETRANS,
+	SM_DPM_END
+} DPM_SM;
+
+/* Workspace definition -------------------------------------------------------*/
+typedef struct WS_DPM
+{
+	I2C_HandleTypeDef *DpmI2cHandle;					/* i2c handle */
+	DPM_RBUF Tx;
+	DPM_SM smTx;										/* state-machine for data transmit */
+	DPM_SM smRx;										/* state-machine for data receive */
+	DPM_BUF halTx;										/* structure for data transmit */
+	DPM_BUF halRx;										/* structure for data receive */
+
+	DPM_STAT busStat;									/* I2C bus status */
+
+
+//	int16_t shuntVal;									/* () measurement shunt value */
+//	int16_t busVal;										/* () measurement bus value */
+
+} WS_DPM;
+
+
+/* Function prototype definition -------------------------------------------------------*/
+void dpm_init(void);
+void dpm_service(void);
+DPM_STAT dpm_write(uint8_t devAddr, uint8_t regAddr, uint8_t *pData, uint8_t size);
+DPM_STAT dpm_writeBuf(uint8_t devAddr, uint8_t regAddr, uint8_t *pData, uint8_t size);
+DPM_STAT dpm_read(uint8_t devAddr, uint8_t regAddr, uint8_t *pData, uint8_t size);
+
+void dpm_transmit(void);
+void dpm_receive(void);
+
+
+#endif /* ISL28023_H_ */
+
+
+
+
+
+
+
